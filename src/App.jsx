50bf43c9270a2bea7a56548863b10ac10b10add5
Kendrick Victor <sunflowerroses801@gmail.com>
2024-04-17 13:31:50 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { SideBar } from "./Layout/SideBar";
 const App = () => {
   return (
     <div className="leading-4 flex items-center flex-col justify-center bg-white h-auto">
-      <div className="max-w-[96%] w-full p-1 ">
+      <div className="max-w-[96%] w-full p-1 overflow-x-hidden ">
         <div className="flex gap-[1px]  bg-gray-200">
           <SideBar />
           <Body />
